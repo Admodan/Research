e760e09bd8a7d27eb7fac61a4835e2fd8d589114
Adam Yars <adamyars@outlook.com>
2020-05-13 10:30:29 -0600

    Added Filtering plans spreadsheet

--- Python Scripts/Filtering Plans Spreadsheet.py
+++ Python Scripts/Filtering Plans Spreadsheet.py
@@ -1,10 +1,14 @@
 import os
+from pathlib import Path
+
 
 file_location = os.path.dirname(os.path.abspath('Filtering Plans Spreadsheet.py'))
 os.chdir(file_location)
 
-os.path.abspath('visvib_pami.pdf')
+os.path.abspath('Pivot Table of Data.xlsx')
 file_location = os.path.dirname(os.path.abspath('Pivot Table of Data.xlsx'))
 with open(os.path.abspath('Pivot Table of Data.xlsx')) as file:
     data = file.read()
 
+Path.cwd()
+os.chdir(Path(r'C:\Users\The Supreme Being\Documents\Research\Research\Python Scripts'))
